60fc02e470408261a19ae846e2ec3969df9d1787
Anghelo Alagon <200518@unsaac.edu.pe>
2023-04-22 18:36:53 -0500

    Refactor a component to improve readability

---
+++ src/components/Banner/Content.jsx
@@ -0,0 +1,15 @@
+export const Content = () => {
+  return (
+    <div class="card-img-overlay  d-flex flex-column justify-content-end container">
+      <h1 class="fw-bold banner__tittle">
+        Febrero Promocional
+      </h1>
+      <p class="card-text fw-bold banner__p">
+        Productos selecionados con 33% de descuento
+      </p>
+      <button className='btn btn-primary banner__button'>
+        Ver Consolas
+      </button>
+    </div>
+  )
+}
--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,5 +1,6 @@
 import banner from '../../assets/banner-image.png';
-import './style.component.css'
+import { Content } from './Content';
+import './style.component.css';
 
 export const Banner = () => {
   return (
@@ -7,19 +8,7 @@ export const Banner = () => {
       <div class="card text-bg-dark">
         <img src={banner} class="card-img banner__img" alt="Banner" />
 
-
-        <div class="card-img-overlay  d-flex flex-column justify-content-end container">
-          <h1 class="fw-bold banner__tittle">
-            Febrero Promocional
-          </h1>
-          <p class="card-text fw-bold banner__p">
-            Productos selecionados con 33% de descuento
-          </p>
-          <button className='btn btn-primary banner__button'>
-            Ver Consolas
-          </button>
-        </div>
-
+        <Content/>
       </div>
 
     </section>
